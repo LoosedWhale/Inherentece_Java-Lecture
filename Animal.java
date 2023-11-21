31a6ba9aeb151c1de1605993028ee07e435d8c8c
LoosedWhale <81869656+LoosedWhale@users.noreply.github.com>
2023-11-21 09:34:15 +0100

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,9 @@
+package animalv2;
+
+public interface Animal {
+	String whatDoesTheAnimalDo();
+	String whatDoesTheAnimalSay();
+	String whatDoesTheAnimalEat();
+	void setName(String name);
+	void setAge(int age);
+}
